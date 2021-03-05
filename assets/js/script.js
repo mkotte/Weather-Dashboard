6281177999a83aa27c9eec528becc6aae8d31eee
mkotte <74936870+mkotte@users.noreply.github.com>
2021-03-05 18:23:51 -0500

    scrapped jquery, started fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,49 +3,51 @@
 const ApiKey = '5a1361306ef906fe293b5db1d3f1f98c'
 // search array to implement / save searches
 let searches = [];
-let searchBtn = $('#searchButton')
+let searchBtn = document.getElementById('search-button')
 let featuredSearchesText = ['Austin', 'Chicago', 'New York', 'Orlando', 'Seattle'];
 let citySearched = "Atlanta";
  
 // On page load, there needs to be a render function using api's for a preset city ie. columbus
 function renderPage(){
     
-    // declaring + appending the container's containing the results and 5-day forecasts
-    let info = $('.information');
-    let results = $('<div>');
-    results.addClass('results');
-    results.appendTo(info);
-    let cityResults = $('<h2>');
-    cityResults.text(citySearched);
-    cityResults.appendTo(results);
-    let tempResults = $('<h3>');
-    tempResults.text = "Temperature: " + temperature + "\u00B0F";
-
     
-    let forecast = $('<div>');
-    forecast.addClass('forecast');
-    forecast.appendTo(info);
-
-
-
     //variables we need to call/fetch from api
-    let temperature = ""
+    let temperature = "";
+    let windSpeed = "";
+    let humidity = "";
+    let uvIndex= "";
+    
+    
+    // declaring + appending elements of the container containing the results
+   
+    
+}
+renderPage();
 
+//function calling api using "fetch off of an event listener"
+let cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&appid=' + ApiKey
+function cityData(){
+    fetch (cityUrl).then( function (response){
+        if(response.ok){
+            response.json().then(function (data){
+                console.log(data)
+                let latitude = data.coord.lat;
+                let longitude = data.coord.lon;
+                console.log(longitude + ", " + latitude)
+
+                weatherData(latitude,longitude)
+            }) 
+        }
+    })
+}
+
+function weatherData(latitude,longitude){
+let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&appid=" + ApiKey;
+    //fetch goes here
 }
 
 
-renderPage();
-
-
-
-
-
-
-
-
-
-//function calling api using "fetch off of an event listener"
-let cityUrl = 'api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&appid=' + ApiKey
+cityData();
 
 //Input btn listener
 
